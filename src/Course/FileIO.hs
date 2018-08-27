fc73232bcc09a1b7fb5d1c33fc305084ae7bd2d8
Alexandre Balon-Perin <alexbp13@gmail.com>
2018-08-27 23:44:26 +0900

    FileIO exercises

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,55 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile f l =
+ putStrLn f *> putStrLn l
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles files = void $ trav (\file -> printFile (fst file) (snd file)) files
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path = (\chars -> (path, chars)) <$> readFile path
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles files = trav getFile files
+
+trav ::
+  (a -> IO b)
+  -> List a
+  -> IO (List b)
+trav f l = sequence $ (<$>) f l
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run filename =
+  do
+    content <- readFile filename
+    results <- getFiles (lines content)
+    printFiles results
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= \args ->
+      case args of
+        filename :. Nil -> run filename
+        _ -> putStrLn "usage: runhaskell io.hs filename"
 
 ----
 
